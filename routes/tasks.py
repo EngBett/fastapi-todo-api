11294a248738880914afb6e2b6d85ecdaffd7a6f
S-cheptoo <cheptoosandra.t@gmail.com>
2022-01-19 15:40:03 +0300

    task delete

--- routes/tasks.py
+++ routes/tasks.py
@@ -42,23 +42,57 @@ async def get_task(task_id: UUID, authorize: AuthJWT = Depends()):
     require_jwt(authorize)
     try:
         task = db.session.query(Task).filter(Task.id == task_id).first()
+        if task is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="task not found")
         return task
     except Exception:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something went wrong")
 
 
-@task_router.put("/task/update/{task_id}")
-async def update_task(task_id: UUID, authorize: AuthJWT = Depends()):
+@task_router.put("/task/update/{task_id}", response_model=TaskModel)
+async def update_task(model: TaskModel, task_id: UUID, authorize: AuthJWT = Depends()):
     require_jwt(authorize)
     try:
-        pass
+        task = db.session.query(Task).filter(Task.id == task_id).first()
+        if task is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="task not found")
+
+        task.title = model.title
+        task.description = model.description
+        task.complete = model.complete
+        db.session.commit()
+        return task
+
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something went wrong")
+
+
+@task_router.put("/task/complete/{task_id}", response_model=TaskModel)
+async def complete_task(task_id: UUID, authorize: AuthJWT = Depends()):
+    require_jwt(authorize)
+    try:
+        task = db.session.query(Task).filter(Task.id == task_id).first()
+        if task is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="task not found")
+
+        task.complete = True
+        db.session.commit()
+        return task
+
     except Exception:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something went wrong")
 
 
 @task_router.delete("/task/delete/{task_id}")
-async def delete_task(task_id: UUID):
+async def delete_task(task_id: UUID, authorize: AuthJWT = Depends()):
+    require_jwt(authorize)
     try:
-        pass
+        task = db.session.query(Task).filter(Task.id == task_id).first()
+        if task is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="task not found")
+        db.session.delete(task)
+        db.session.commit()
+        return {"message", "successfully deleted"}
+
     except Exception:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something went wrong")
